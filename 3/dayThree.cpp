cc94aef6bd5b4dd92e075ae44e32b629006297d4
enterCheesyNameHere <gwdeibler@gmail.com>
2023-12-04 00:35:25 -0500

    Started day 3

---
+++ 3/dayThree.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+const string PATH = getenv("SOURCEDIR");
+
+int main() {
+    ifstream FInput(PATH + "/input.txt");
+    string line;
+
+    return 0;
+}
